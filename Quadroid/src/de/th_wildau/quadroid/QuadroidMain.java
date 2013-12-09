7d8d861d172b1c1394ae12c44b345c360d971f7b
Tommy <BecksBoot1@googlemail.com>
2013-12-09 20:45:04 +0100

    Projekt modifiziert, schnitstellen fuer das senden und empfangen erstellt

--- Quadroid/src/de/th_wildau/quadroid/QuadroidMain.java
+++ Quadroid/src/de/th_wildau/quadroid/QuadroidMain.java
@@ -1,18 +1,8 @@
 package de.th_wildau.quadroid;
 
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import de.th_wildau.quadroid.converter.DataConverter;
-import de.th_wildau.quadroid.handler.ReceiverHandler;
-import de.th_wildau.quadroid.handler.TransmitterHandler;
-import de.th_wildau.quadroid.interfaces.IReceiver;
+
 
 
 /**
@@ -24,63 +14,21 @@ import de.th_wildau.quadroid.interfaces.IReceiver;
  * 
  * */
 
-public class QuadroidMain extends JFrame implements IReceiver
+public class QuadroidMain
 {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	private static QuadroidMain qm = null;
 	
-	private static BufferedImage rximage = null;
 	
 	/**Logger for Logging with {@link org.slf4j.Logger}*/
 	public static Logger logger = null;
 	
-	private static DataConverter dc = null;
-	
-	/**Main Method*/
+
+
 	public static void main(String[] args)
 	{
-		qm = new QuadroidMain();
 		logger = LoggerFactory.getLogger(QuadroidMain.class.getName());
-		ReceiverHandler rh = new ReceiverHandler();
-		TransmitterHandler th = new TransmitterHandler();
-		rh.addIReceiverObserver(qm);
 		
-		qm.setBounds(100, 100, 500, 500);
-		qm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		try 
-		{
-			BufferedImage image = ImageIO.read(new File("Oktoberfest.jpg"));
-			dc = new DataConverter();
-			th.transmit(dc.convertImageToByteArray(image, "jpg"));
-		} catch (IOException e) { 
-			e.printStackTrace();
-			System.exit(0);
-		}
-		
-	}
 
-	@Override
-	public void paint(Graphics g) 
-	{
-		if(rximage != null)
-		{
-			qm.setBounds(100, 100, rximage.getWidth(), rximage.getHeight());
-			g.drawImage(rximage, 0, 0, rximage.getWidth(), rximage.getHeight(), null);
-		}
-	}
-	
-	@Override
-	public void dataInputNotyfier(byte[] data) 
-	{
-		qm.setVisible(false);
-		rximage = dc.convertByteToBufferedImage(data);
-		qm.repaint();
-		qm.setVisible(true);
+		
 	}
 
 	
--- Quadroid/src/de/th_wildau/quadroid/converter/DataConverter.java
+++ Quadroid/src/de/th_wildau/quadroid/converter/DataConverter.java
@@ -4,9 +4,12 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.zip.CRC32;
+
 import javax.imageio.ImageIO;
 import org.slf4j.Logger;
 import de.th_wildau.quadroid.QuadroidMain;
+import de.th_wildau.quadroid.enums.Marker;
 
 /**
  * This Class convert different objects to bytes 
@@ -109,5 +112,95 @@ public class DataConverter
 		return bufferedimage;
 	}
 
+	/**
+	 * this function compute the crc32 checksum, for data between startmarker and
+	 * endmarker. 
+	 * 
+	 * @param data <p>hand over an array of bytes, contains data enclose with 
+	 * startmaker and endmarker. For this data will be compute the crc32 
+	 * checksum, additional for compare data must be contain an transmit checksum 
+	 * enclose with CRCSTART and CRCEND see {@link de.th_wildau.quadroid.enums.Marker} 
+	 * </p>
+	 * 
+	 * @param startmarker <p>marked begin of data for computing crc32 checksum</p>
+	 * 
+	 * @param endmarker <p>marked end of data for computing crc32 checksum</p>
+	 * 
+	 * @return 
+	 * <ul>
+	 * <li>true = computed CRC32 equals with contained</li>
+	 * <li>false = computed CRC32 unequal with contained</li>
+	 * or
+	 * <li>false = no contained CRC32 enclosed with CRCSTART and CRCEND</li>
+	 * or
+	 * <li>false = marker out of bounds</li>
+	 * or
+	 * <li>false = reference of data <b>null</b></li>
+	 * </ul>
+	 * 
+	 * */
+	public boolean proveCRC(byte[] data, String startmarker, String endmarker)
+	{
+		if(data == null)//prove data
+		   return false;
+		CRC32 crc32 = new CRC32();//create instance of crc32
+		try
+		{
+			String convert = new String(data);//convert to string for easy search marker
+			//search index for start and end marker and return substring between markers
+			//if no available an exception will be throw
+			String crcdata = convert.substring(convert.indexOf(startmarker), 
+							  				   convert.indexOf(endmarker));
+			
+			logger.debug("StartMarkerOfData: " + convert.indexOf(startmarker) + " " +
+						 "EndMarkerOfData: " + convert.indexOf(endmarker));
+			// -||-
+			String crc = convert.substring(convert.indexOf(Marker.CRCSTART.getMarker()), 
+										   convert.indexOf(Marker.CRCEND.getMarker()));
+			logger.debug("StartMarkerOfCRC: " + convert.indexOf(Marker.CRCSTART.getMarker()) + " " +
+					     "EndMarkerOfCRC: " + convert.indexOf(Marker.CRCEND.getMarker()));
+			//convert so long for comparing
+			long containscrc = Long.valueOf(crc);
+			logger.debug("Contains CRC: " + containscrc);
+			//compute crc over substring
+			crc32.update(crcdata.getBytes());
+			//get crc checksum 
+			long camputedcrc = crc32.getValue();
+			logger.debug("Computed CRC: " + camputedcrc);
+			//compare checksum
+			if(containscrc == camputedcrc)
+			{
+				logger.info("CRC32 OK");
+				return true;//if equals return true
+			}
+			logger.info("CRC32 Unequal");
+		}catch(Exception e)
+		{
+			logger.error("CRC32 Exception: " , e);
+			return false;//
+		}	
+		
+		return false;
+	}
 
+	public static void main(String[] val)
+	{
+		String data = Marker.AKKUSTART.getMarker() + "1234567890" + Marker.AKKUEND.getMarker();
+		CRC32 crc = new CRC32();
+		
+		
+		crc.update(data.getBytes(), data.indexOf(Marker.AKKUSTART.getMarker()) + 3, data.indexOf(Marker.AKKUEND.getMarker()) - 1);
+		
+		//System.out.println( "Index: " + data.charAt(data.indexOf(Marker.AKKUSTART.getMarker()) + 3) + "  " + data.charAt(data.indexOf(Marker.AKKUEND.getMarker()) - 1));
+		
+		String crc32 = String.valueOf(crc.getValue());
+		
+		data += Marker.CRCSTART.getMarker() + crc32 + Marker.CRCEND.getMarker();
+		
+		System.out.println( data );
+		
+		DataConverter dc = new DataConverter();
+		System.out.println( dc.proveCRC(data.getBytes(), Marker.AKKUSTART.getMarker(), Marker.AKKUEND.getMarker()) );
+	}
+	
 }
