598be4dbd16d2f0f615bd0e2b52a89c72db431ff
Snerx <stephan.funke@th-wildau.de>
2014-01-12 11:37:29 +0100

    added some comments to landmark testcase

--- Quadroid/src/de/th_wildau/quadroid/test/landmark/TestOpCheckLandmark.java
+++ Quadroid/src/de/th_wildau/quadroid/test/landmark/TestOpCheckLandmark.java
@@ -18,15 +18,16 @@ public class TestOpCheckLandmark extends TestCase {
 		try {
 			System.loadLibrary("opencv_java245");
 
-			BufferedImage positivTest = ImageIO.read( new File( "testKamera.png" ) );
-			BufferedImage negativTest = ImageIO.read( new File( "k-DSC_4243.jpg" ) );
+			BufferedImage positivTest = ImageIO.read( new File( "testKamera.png" ) );//Load Testpicture with landmark
+			BufferedImage negativTest = ImageIO.read( new File( "k-DSC_4243.jpg" ) );//Load Testpicture without landmark
 			MainLandmark lm = new MainLandmark();
+			//Test positive Picture
 			assertTrue(lm.checkLandmark(positivTest));
+			//Test negative Picture
 			assertFalse(lm.checkLandmark(negativTest));
 			
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
